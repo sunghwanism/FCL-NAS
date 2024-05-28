8aa0150b8f44bf63cb13f28363506c89ed7f19c2
sunghwanism <97149338+sunghwanism@users.noreply.github.com>
2024-05-28 18:17:34 -0400

    update script file for FedNAS

--- src/mpi_torch_fednas_cifar10_dart_example/run_fednas_train.sh
+++ src/mpi_torch_fednas_cifar10_dart_example/run_fednas_train.sh
@@ -9,5 +9,6 @@ echo $PROCESS_NUM
 hostname > mpi_host_file
 
 mpirun -np $PROCESS_NUM \
--hostfile mpi_host_file --oversubscribe \
+-hostfile mpi_host_file \
+--oversubscribe \
 python torch_fednas_cifar10_dart_step_by_step_example.py --cf config/train_config.yaml
